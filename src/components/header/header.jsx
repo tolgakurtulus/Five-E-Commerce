97993fe081ac3fc1041457a3ae6d6ed576316b9d
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2021-10-24 13:19:39 +0300

    Chance Commerce İcon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
-import { ReactComponent as Logo} from "../../assets/crown.svg";
+import { ReactComponent as Logo} from "../../assets/fivecommerce.svg";
 import { connect } from "react-redux";
 import "./header.scss";
 import CartIcon from "../cartIcon/cartIcon";
